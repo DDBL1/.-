a32f1b4bf6326887449d36dcf17a81ab33fb77f5
DDBL1 <103415942+DDBL1@users.noreply.github.com>
2022-06-22 14:47:18 +0700

    Update 1.lua

--- ..-.,/1.lua
+++ ..-.,/1.lua
@@ -158,7 +158,7 @@ local library = {
 		guiname = game.PlaceId,
 		title = "[XXXIV-a] Allusions [Exodus Edition]",
 		logo = "rbxassetid://4350178803", -- "http://www.roblox.com/asset/?id=6597976562",
-		footer = "Version 1.0",
+		footer = "Version 1.1",
 		modal = true,
 		font = Enum.Font.SourceSans,
 		textsize = 16,
